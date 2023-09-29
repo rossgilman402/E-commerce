48b2abdd996a769d910f6f75c9591f5724db89a0
ross402 <65184393+ross402@users.noreply.github.com>
2023-09-29 16:01:18 -0400

    Finished tag routes

--- models/index.js
+++ models/index.js
@@ -6,27 +6,29 @@ const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: "id",
+  foreignKey: "category_id",
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: "id",
+  foreignKey: "category_id",
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
+  foreignKey: "product_id",
   through: {
     model: ProductTag,
-    unique: false,
+    // unique: false,
   },
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
+  foreignKey: "tag_id",
   through: {
     model: ProductTag,
-    unique: false,
+    // unique: false,
   },
 });
 
